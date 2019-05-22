bebacb7be5595e9a5496b9909273d904c42919d0
ShyGuyPy <lvawter@icprb.org>
2019-05-22 10:42:22 -0400

    retrieve working, requirements.txt updated

--- Retrieve_NWM.py
+++ Retrieve_NWM.py
@@ -1,57 +1,27 @@
-from traceback import format_exc
-import sys
-#import FEWSConnect
-import logging
-from paramiko import Transport
-from paramiko import SFTPClient
-import os
-import stat
-import paramiko
 
 
-
-
-
-host = 'www.ftp.ncep.noaa.gov'
-site_path = '/data/nccf/com/nwm/prod/nwm.20190515/short_range/'
-
-#host = 'ftp://ftp.ncep.noaa.gov'
-#site_path = '/pub/data/nccf/com/nwm/prod/nwm.20190515/short_range/'
-
-port = 21  # after much testing....port 22 is the place to be
-
-lookback = 5
-
-destination = r"C:\Users\Public\Documents\Python Scripts\file_destination"
-file_name = 'nwm.t00z.short_range.channel_rt.f001.conus.nc'
-
-importDir = "C:/Users/Public/Documents/Python Scripts/file_destination"  # open(destination + file_name + extension, "wb")
-
-
-
-
-
-#Use requests to get page content and BeautifulSoup to parse the result.
-#For example if we search for all iso files at http://cdimage.debian.org/debian-cd/8.2.0-live/i386/iso-hybrid/:
 
 from bs4 import BeautifulSoup
 import requests
 import wget
-from datetime import datetime
+from datetime import datetime, timedelta
 
 date_today = datetime.today().strftime('%Y%m%d')
+date_yesterday = datetime.strftime(datetime.now() - timedelta(1), '%Y%m%d')
 
+#the http site for NWM products
 site_path = 'https://www.ftp.ncep.noaa.gov/data/nccf/com/nwm/prod/nwm.'
 
 
 data_range = '/short_range/'
 
-file = 'nwm.t00z.short_range.channel_rt.f001.conus.nc'
+#creates a url to pull yesterdays data
+url = site_path+date_yesterday+data_range
 
-url = site_path+date_today+data_range
-
+#netcdf extension
 ext = 'nc'
 
+#creates list of url directory content
 def listFD(url, ext=''):
     page = requests.get(url).text
     #print page
@@ -60,6 +30,7 @@ def listFD(url, ext=''):
 
 test_list = []
 
+#finds the last hour that data was recorded.  should be 23
 for file in listFD(url, ext):
     if file.find("t23z.short_range.channel") >1: #& file.find("t17z") >1 :
         max_hour = "t23z"
@@ -76,89 +47,14 @@ for file in listFD(url, ext):
     elif  file.find("t17z.short_range.channel") >1: #& file.find("t17z") >1 :
         max_hour = "t17z"
 
-
-
-print(max_hour)
-
+#append data found at last hour of day to a separate list
 for file in listFD(url, ext):
-    #print file
     if file.find(max_hour+".short_range.channel") >1: #& file.find("t17z") >1 :
         test_list.append(file)
 
-#print(test_list)
+#sloppy naming convention...this downloads the data to a designated directory
 count = 0
 for file in test_list:
     count = count +1
-    test_url = str(test_list)
-    print(test_url)
-    print('/Users/icprbadmin/Documents/Python_Scripts/NWM/data/5_21_2019/test/'+str(date_today)+'test_data'+str(count)+'.nc')
-    wget.download(test_url, '/Users/icprbadmin/Documents/Python_Scripts/NWM/data/5_21_2019/test/'+str(date_today)+'test_data'+str(count)+'.nc')
-
-#url = 'https://www.ftp.ncep.noaa.gov/data/nccf/com/nwm/prod/nwm.20190521/short_range/nwm.t00z.short_range.channel_rt.f001.conus.nc'
-#wget.download(test_url, '/Users/icprbadmin/Documents/Python_Scripts/NWM/data/5_21_2019/test/test.nc')
-
-# 1
-#try:
-
-# transport = Transport((host, port))
-# transport.connect()
-# sftp = SFTPClient.from_transport(transport)
-# print("1 it's alive...sftp object up and running")
-# except:
-#     print("1 failed to connect")
-
-
-
-#
-#
-#
-# # 2
-# try:
-#     # Retrieve list of files from sftp site
-#     s = sftp.listdir(site_path)
-#     # print("2 These are the files in " + host )#+ path + ": ")
-#     # print(s)
-# except:
-#     print("2 no good retrieving file list")
-# #
-# # t=sftp.listdir(path)
-# # 3
-# try:
-#     hrsback = int(lookback) * 24
-#     # List of files in directory
-#     allfiles = [filename for filename in s if filename.endswith('.txt')][-hrsback:]
-#     print('3' + allfiles)
-# except:
-#     print("3 no good finding file")
-# # 4
-# try:
-#     for file in allfiles:
-#         reader = sftp.open(site_path + file, 'rb').read()
-#         # print(pdf)
-#         print("4 read successful")
-#
-# except:
-#     print("4 no good on read from file")
-#
-# # 5
-# try:
-#     for file in allfiles:
-#         #        Store file in FEWS import Directory
-#         with open(importDir + file, 'wb') as f:
-#             f.write(reader)
-#     print("5 file saved to " + importDir + file)
-# except:
-#     print("5 no good on file save attempt 1")
-#     # print(pdf)
-# # 6
-# try:
-#     sftp.close()
-#     print("6 closed sftp object")
-# except:
-#     print("6 sftp did not close")
-# # 7
-# try:
-#     f.close()  # importDir.close()
-#     print("7 closed importDir object")
-# except:
-#     print("7 importDir did not close")
+    test_url = str(file)
+    wget.download(test_url, '/Users/icprbadmin/Documents/Python_Scripts/NWM/data/5_21_2019/test/'+str(date_yesterday)+'test_data'+str(count)+'.nc')
